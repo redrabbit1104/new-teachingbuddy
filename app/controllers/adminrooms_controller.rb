e52c38f5d3c16d0f641b126666ba9a65a816ea4e
redrabbit1104 <redcaprabbit1104@gmail.com>
2021-03-18 08:49:40 +0900

    チャットルームバグ修正

--- app/controllers/adminrooms_controller.rb
+++ app/controllers/adminrooms_controller.rb
@@ -22,7 +22,7 @@ class AdminroomsController < ApplicationController
   def create
     @admin_room = AdminRoom.new(room_params)
     @admin_room.save
-    redirect_to adminroom_adminmessages_path(AdminRoom.last.id)
+    redirect_to adminroom_adminmessages_path(AdminRoom.find_by(params[:admin_room][:user_id]))
   end
 
   def destroy
