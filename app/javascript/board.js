b439a51fa4c42cd9a88bc83c91dda61d851727ae
redrabbit1104 <redcaprabbit1104@gmail.com>
2021-05-01 11:47:25 +0900

    modify board style

--- app/javascript/board.js
+++ app/javascript/board.js
@@ -1,31 +1,33 @@
 function board() {
-    const submit = document.getElementById("submit");
-    if (!submit) { return false; }
-    submit.addEventListener("click", (e) => {
-        const formData = new FormData(document.getElementById("form"));
-        const XHR = new XMLHttpRequest();
-        XHR.open("POST", "/boards", true);
-        XHR.responseType = "json";
-        XHR.send(formData);
-        XHR.onload = () => {
-            if (XHR.status != 200) {
-                alert(`Error ${XHR.status}: ${XHR.statusText}`);
-                return null;
-            }
-            const item = XHR.response.board;
-            const list = document.getElementById("list");
-            const formText = document.getElementById("post");
-            const HTML = `
+  const submit = document.querySelector(".submit_comment");
+  if (!submit) {
+    return false;
+  }
+  submit.addEventListener("click", (e) => {
+    const formData = new FormData(document.querySelector(".comment_form"));
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/boards", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      }
+      const item = XHR.response.board;
+      const list = document.getElementById("list");
+      const formText = document.querySelector(".input_comment");
+      const HTML = `
                 <td class="post">${item.user_id}</td>
                 <td class="post-date">${item.created_at}</td>
                 <td class="post-content">${item.post}</td>
                 <td></td>
                 <td></td>
                 `;
-            list.insertAdjacentHTML("afterend", HTML);
-            formText.value = " ";
-        };
-        e.preventDefault();
-    });
-};
+      list.insertAdjacentHTML("afterend", HTML);
+      formText.value = " ";
+    };
+    e.preventDefault();
+  });
+}
 window.addEventListener("load", board);
