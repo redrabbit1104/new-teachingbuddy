341775cae17c53581ce7842dac09379688b03d30
redrabbit1104 <redcaprabbit1104@gmail.com>
2021-05-02 16:23:02 +0900

    modify board.js

--- app/javascript/board.js
+++ app/javascript/board.js
@@ -5,29 +5,35 @@ function board() {
   }
   submit.addEventListener("click", (e) => {
     const formData = new FormData(document.querySelector(".comment_form"));
-    const XHR = new XMLHttpRequest();
-    XHR.open("POST", "/boards", true);
-    XHR.responseType = "json";
-    XHR.send(formData);
-    XHR.onload = () => {
-      if (XHR.status != 200) {
-        alert(`Error ${XHR.status}: ${XHR.statusText}`);
-        return null;
-      }
-      const item = XHR.response.board;
-      const list = document.getElementById("list");
-      const formText = document.querySelector(".input_comment");
-      const HTML = `
-                <td class="post">${item.user_id}</td>
-                <td class="post-date">${item.created_at}</td>
-                <td class="post-content">${item.post}</td>
-                <td></td>
-                <td></td>
-                `;
-      list.insertAdjacentHTML("afterend", HTML);
-      formText.value = " ";
+    const url = "/boards";
+    const post_options = {
+      method: "post",
+      body: formData,
     };
+    fetch(url, post_options)
+      .then((response) => {
+        if (!response.ok) {
+          console.log(`Error ${reponse.status}: ${reponse.statusText}`);
+          return null;
+        } else {
+          return response.json();
+        }
+      })
+      .then((item) => {
+        const list = document.getElementById("list");
+        const formText = document.querySelector(".input_comment");
+        const HTML = `
+                    <td class="post">${item.board.user_id}</td>
+                    <td class="post-date">${item.board.created_at}</td>
+                    <td class="post-content">${item.board.post}</td>
+                    <td></td>
+                    <td></td>
+                    `;
+        list.insertAdjacentHTML("afterend", HTML);
+        formText.value = " ";
+      });
     e.preventDefault();
   });
 }
+
 window.addEventListener("load", board);
